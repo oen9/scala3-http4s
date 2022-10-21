0645aa14bbfdb39bde34391c141d8bd577c594b1
oen <oen.ult@gmail.com>
2022-10-21 13:24:57 +0200

    commented old logger

--- src/main/scala/Logging.scala
+++ src/main/scala/Logging.scala
@@ -6,3 +6,8 @@ import org.typelevel.log4cats.slf4j.Slf4jLogger
 trait Logging {
   given [F[_]: Async]: SelfAwareStructuredLogger[F] = Slf4jLogger.getLoggerFromClass(getClass)
 }
+//import org.typelevel.log4cats.Logger
+//trait Logging[F[_]: Sync] {
+//  given SelfAwareStructuredLogger[F] = Slf4jLogger.getLoggerFromClass(getClass)
+//  def log = Logger[F]
+//}
